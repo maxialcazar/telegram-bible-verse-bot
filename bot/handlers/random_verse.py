8889f0b23a15a6cedd032c38bcdbf9f33eb5518a
maxialcazar <32802965+maxialcazar@users.noreply.github.com>
2025-11-21 15:57:19 -0300

    Add /books and /help commands to bot
    Introduces a new /books command to list available book IDs and a /help command to display usage instructions. Updates random verse handler for improved response formatting and registers the new commands in main.py.

--- bot/handlers/random_verse.py
+++ bot/handlers/random_verse.py
@@ -4,16 +4,20 @@ from bot.config import API_URL
 async def random_command(update, context):
     try:
         response = requests.get(f"{API_URL}data/web/random")
-        data = response.json()
+        data = response.json()["random_verse"]
 
-        verse = data.get("text", "")
-        reference = data.get("reference", "")
+        text = data.get("text", "")
+        book = data.get("book", "")
+        chapter = data.get("chapter", "")
+        verse = data.get("verse", "")
+        reference = f"{book} {chapter}:{verse} :"
+
 
         if not verse:
             await update.message.reply_text("No se pudo obtener un versículo aleatorio.")
             return
 
-        await update.message.reply_text(f"{reference}\n\n{verse}")
+        await update.message.reply_text(f"{reference}\n\n{text}")
 
     except Exception as e:
         await update.message.reply_text(f"Error: {e}")
--- bot/main.py
+++ bot/main.py
@@ -3,6 +3,8 @@ from bot.handlers.chapter import chapter_command
 from bot.handlers.verse import verse_command
 from bot.handlers.langs import langs_command
 from bot.handlers.random_verse import random_command
+from bot.handlers.books_id import books_command
+from bot.handlers.help import help_command
 
 from bot.config import TOKEN
 
@@ -14,9 +16,10 @@ def main():
     app.add_handler(CommandHandler("chapter", chapter_command))
     app.add_handler(CommandHandler("langs", langs_command))
     app.add_handler(CommandHandler("random", random_command))
+    app.add_handler(CommandHandler("books", books_command))
+    app.add_handler(CommandHandler("help", help_command))
 
     app.run_polling()
-
 
 if __name__ == "__main__":
     main()
